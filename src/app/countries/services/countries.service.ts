74af30c28c5329c62fec2a3d2f60f06682594407
sebastian461 <sebastiano-461@hotmail.com>
2023-07-07 07:46:32 -0500

    Catch Error
    Lookup errors are caught through a pipe property in the observer that is in the country service.

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({ providedIn: 'root' })
@@ -10,6 +10,8 @@ export class CountryService {
   constructor(private http: HttpClient) {}
 
   searchCapital(searchTerm: string): Observable<Country[]> {
-    return this.http.get<Country[]>(`${this.apiUrl}/capital/${searchTerm}`);
+    return this.http
+      .get<Country[]>(`${this.apiUrl}/capital/${searchTerm}`)
+      .pipe(catchError(() => of([])));
   }
 }
