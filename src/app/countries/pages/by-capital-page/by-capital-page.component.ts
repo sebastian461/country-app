c9b8f78d8258c2cf9249fa742f61669ed67d1785
sebastian461 <sebastiano-461@hotmail.com>
2023-07-09 02:06:34 -0500

    Loader spinner
    Added the logic for the load component to appear

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -9,12 +9,16 @@ import { Country } from '../../interfaces/country';
 })
 export class ByCapitalPageComponent {
   public countries: Country[] = [];
+  public isLoading: boolean = false;
 
   constructor(private countryService: CountryService) {}
 
   searchByCapital(term: string): void {
+    this.isLoading = true;
+
     this.countryService.searchCapital(term).subscribe((countries) => {
       this.countries = countries;
+      this.isLoading = false;
     });
   }
 }
