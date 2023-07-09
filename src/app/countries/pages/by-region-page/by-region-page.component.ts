e645ed32b8d0e0f7ace712a2790d2996bdf5b845
sebastian461 <sebastiano-461@hotmail.com>
2023-07-09 17:09:08 -0500

    Revert "Serach by region"
    This reverts commit a1b8fb94ad0805d5353f7ba882c8c323558592ed.

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountryService } from '../../services/countries.service';
 
-type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
-
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -12,21 +10,9 @@ type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
 export class ByRegionPageComponent {
   public countries: Country[] = [];
 
-  public regions: Region[] = [
-    'Africa',
-    'Americas',
-    'Asia',
-    'Europe',
-    'Oceania',
-  ];
-
-  public selectedRegion?: Region;
-
   constructor(private countryService: CountryService) {}
 
-  searchByRegion(term: Region): void {
-    this.selectedRegion = term;
-
+  searchByRegion(term: string): void {
     this.countryService.searchRegion(term).subscribe((countries) => {
       this.countries = countries;
     });
